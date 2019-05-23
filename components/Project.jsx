c9d3bc5dbd51628d2a5cbf294a088a90b7c8351b
Phumrapee Limpianchop <git@rayriffy.com>
2019-05-24 02:38:44 +0700

    :zap: Server-side rendering

--- components/Project.jsx
+++ components/Project.jsx
@@ -20,13 +20,13 @@ const Project = props => {
         <Box mx='auto' width={[23 / 24, 23 / 24, 22 / 24, 21 / 24]}>
           <Flex flexWrap='wrap'>
             {projects.map(project => (
-              <Box width={[1, 1 / 2, 1 / 3, 1 / 3]} px={10} py={2} key={`lang-${project.name}`}>
+              <Box width={[1, 1 / 2, 1 / 3, 1 / 3]} px={10} py={2} key={`proj-${project.name}`}>
                 <Card borderRadius={6} p={20} boxShadow='8px 14px 38px rgba(39,44,49,.06), 1px 3px 8px rgba(39,44,49,.03)'>
                   {project.slug ? <Text fontSize={12} color='red'>{project.slug}</Text> : null}
                   <Text fontWeight={500}>{project.name}</Text>
                   <Text fontSize={14} color={`rgba(0, 0, 0, 0.50)`}>{project.year.start}{project.year.end || project.year.end === null ? ` - ${project.year.end === null ? `Now` : project.year.end}` : null}</Text>
                   <Text fontSize={14} color='hsla(0,0%,0%,0.8)' mt={10}>{project.links.map(link => (
-                    <><Link href={link.href}>{link.name}</Link> </>
+                    <div key={`proj-${project.name}-${link.name}`}><Link href={link.href}>{link.name}</Link> </div>
                   ))}</Text>
                 </Card>
               </Box>
