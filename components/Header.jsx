d3f65bc849c5c5e1ed2fbde196f337952acefdab
Phumrapee Limpianchop <git@rayriffy.com>
2019-06-11 14:59:36 +0700

    :memo: Change subtitle

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       <Box mx="auto" width={[23 / 24, 23 / 24, 20 / 24, 20 / 24]}>
         <Heading fontSize={42}>Phumrapee Limpianchop</Heading>
         <Text color={`rgba(0, 0, 0, 0.75)`} pt={3}>
-          Student at Faculty of ICT, Mahidol University
+          Lyrr viva tou conquieta, Aeterrno amora
         </Text>
       </Box>
     </Flex>
