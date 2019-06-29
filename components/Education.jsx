2b15896081cba024e63c1b8d7f113dc9a616dcea
Phumrapee Limpianchop <git@rayriffy.com>
2019-06-29 11:41:37 +0700

    :art: Change border radius

--- components/Education.jsx
+++ components/Education.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
+import styled from 'styled-components'
 import {Flex, Box, Text, Card, Image} from 'rebass'
 
 import Title from './Title'
 
 import {educations} from '../data/database'
+
+const Img = styled(Image)`
+  border-radius: 6px 6px 0 0;
+`
 
 const Education = () => {
   return (
@@ -16,7 +21,7 @@ const Education = () => {
             {educations.map(education => (
               <Box width={[1, 1 / 2, 1 / 3, 1 / 3]} px={10} py={2} key={`edu-${education.name}`}>
                 <Card borderRadius={6} boxShadow="8px 14px 38px rgba(39,44,49,.06), 1px 3px 8px rgba(39,44,49,.03)">
-                  <Image src={education.banner} borderRadius={6} />
+                  <Img src={education.banner} borderRadius={6} />
                   <Box p={20}>
                     <Text fontWeight={500}>{education.name}</Text>
                     <Text fontSize={14} color="rgba(0, 0, 0, 0.6)">
