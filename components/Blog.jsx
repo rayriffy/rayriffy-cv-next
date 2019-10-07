0b7fbc956b05fa919e763ef1c127dc6bd4f10b6b
Phumrapee Limpianchop <git@rayriffy.com>
2019-10-07 21:30:14 +0700

    :sparkles: Staged changes

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -9,23 +9,18 @@ import Title from './Title'
 const Blog = () => {
   const [error, setError] = useState(false)
   const [raw, setRaw] = useState(null)
-  const [fetch, setFetch] = useState(false)
 
   useEffect(() => {
-    if (fetch === false && error === false && raw === null) {
-      axios
-        .get('https://blog.rayriffy.com/api/author/rayriffy.json')
-        .then(out => {
-          setRaw(out.data)
-          setFetch(false)
-        })
-        .catch(err => {
-          console.log(err)
-          setError(true)
-          setFetch(false)
-        })
-    }
-  }, [raw, error, fetch])
+    axios
+      .get('https://blog.rayriffy.com/api/author/rayriffy/1.json')
+      .then(out => {
+        setRaw(out.data.data)
+      })
+      .catch(err => {
+        console.log(err)
+        setError(true)
+      })
+  }, [])
 
   return (
     <Box>
@@ -39,12 +34,12 @@ const Blog = () => {
               <Box width={1}>Loading</Box>
             ) : (
               raw.map(blog => (
-                <Box width={[1, 1, 1 / 2, 1 / 2]} px={10} py={2} key={`blog-${blog.name}`}>
-                  <Link href={blog.slug}>
+                <Box width={[1, 1, 1 / 2, 1 / 2]} px={10} py={2} key={`blog-${blog.title}`}>
+                  <Link href={blog.url}>
                     <Card borderRadius={6} boxShadow="8px 14px 38px rgba(39,44,49,.06), 1px 3px 8px rgba(39,44,49,.03)" backgroundImage={`url(${blog.banner})`} backgroundSize="cover">
                       <Card px={4} pb={4} pt={'25%'} color="white" bg="rgba(0,0,0,0.2)" borderRadius={8}>
                         <Text fontWeight={500} textAlign="center">
-                          {blog.name}
+                          {blog.title}
                         </Text>
                       </Card>
                     </Card>
